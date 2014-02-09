ea182109d562a0bf95370c84594f64d8eaf41d0c
John Pulford <johnpulford@gmail.com>
2014-02-09 18:21:08 +0000

    Begun moving logic into antlr4 plugin.

--- buildSrc/antlr4/src/main/groovy/antlr4/Antlr4.groovy
+++ buildSrc/antlr4/src/main/groovy/antlr4/Antlr4.groovy
@@ -3,12 +3,35 @@ package antlr4
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.tasks.compile.JavaCompile
+import org.gradle.api.tasks.testing.Test
 
 public class Antlr4 implements Plugin<Project> {
 
     void apply(Project project) {
-        project.tasks.create('dummyname1', JavaCompile) << {
+        print"apply task running."
+        //project.tasks.create('dummyname1', Compile) << {
+        //    options.encoding = 'UTF-8'
+        //    println "Antlr plugin compile time task."
+        //}
+        
+        //project.tasks.withType(JavaCompile)
+        project.tasks.withType(JavaCompile) {
+            options.encoding = 'UTF-8'
         }
+
+        project.tasks.withType(Test) {
+            systemProperties = System.getProperties()
+            testLogging.showStandardStreams = true
+        }
+        /*
+        project.tasks.create('antlrOutputDir') << {
+            mkdir(antlr.destinationDir)
+        }
+        */
+        //def compileTask = project.tasks.withType(JavaCompile)
+        //print compileTask.conventionMapping.type
+        //compileTask.options.encoding = 'UTF-8'
+        //compileTask.conventionMapping.options.encoding = 'UTF-8'
     }
 
 }
